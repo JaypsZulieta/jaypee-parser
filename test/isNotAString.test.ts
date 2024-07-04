1dc4d985d9103449c9d8d72b6b27d716cde6f666
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-05 01:53:03 +0800

    test: Add test for isNotAString function

---
+++ test/isNotAString.test.ts
@@ -0,0 +1,13 @@
+import { isNotAString } from "../src/validation-fucntions";
+
+describe("isNotAString", () => {
+  test("Should return true if the data passed is NOT a string.", () => {
+    const data = 69 as unknown;
+    expect(isNotAString(data)).toBe(true);
+  });
+
+  test("Should return false if the data passed IS a string.", () => {
+    const data = "Foo" as unknown;
+    expect(isNotAString(data)).toBe(false);
+  });
+});
