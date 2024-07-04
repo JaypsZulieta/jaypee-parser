ed8ddef20232311ffa7b0e27700f413417fba38a
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-05 02:16:27 +0800

    feat: Add isNotABoolean helper function implementaion
    Add this function as a helper for the main feature.

--- src/validation-fucntions.ts
+++ src/validation-fucntions.ts
@@ -8,3 +8,8 @@ export function isNotANumber(data: unknown): boolean {
   if (!isNotAString(data)) return true;
   return !Zod.number().safeParse(data).success;
 }
+
+export function isNotABoolean(data: unknown): boolean {
+  if (!isNotANumber(data)) return !((data as number) == 1 || (data as number) == 0);
+  return !Zod.boolean().safeParse(data).success;
+}
