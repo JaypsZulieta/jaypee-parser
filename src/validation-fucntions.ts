709aadf70b3910fc558376c40487d3b859a64c00
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-05 02:04:19 +0800

    feat: Add isNotANumber helper function impementaion
    Add this function as a helper for the main feature

--- src/validation-fucntions.ts
+++ src/validation-fucntions.ts
@@ -3,3 +3,8 @@ import { z as Zod } from "zod";
 export function isNotAString(data: unknown): boolean {
   return !Zod.string().safeParse(data).success;
 }
+
+export function isNotANumber(data: unknown): boolean {
+  if (!isNotAString(data)) return true;
+  return !Zod.number().safeParse(data).success;
+}
