bb357bd8c2d94dc436e13f5d09b18e94d9408989
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-05 02:12:49 +0800

    test: Add test for isNotABoolean helper function

---
+++ test/isNotABoolean.test.ts
@@ -0,0 +1,23 @@
+import { isNotABoolean, isNotANumber } from "../src/validation-fucntions";
+
+describe("isNotABoolean", () => {
+  test("Should return true if the data passed is NOT a boolean", () => {
+    const data = "Foo" as unknown;
+    expect(isNotABoolean(data)).toBe(true);
+  });
+
+  test("Should return false if the data passed is number 1", () => {
+    const data = 1 as unknown;
+    expect(isNotANumber(data)).toBe(false);
+  });
+
+  test("Should return false if the data passed is number 0", () => {
+    const data = 0 as unknown;
+    expect(isNotANumber(data)).toBe(false);
+  });
+
+  test("Should return false if the data passed IS a boolean", () => {
+    const data = true as unknown;
+    expect(isNotABoolean(data)).toBe(false);
+  });
+});
