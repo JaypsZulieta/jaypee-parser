41456b2a85f92e40cd4b0cf58ea5529deed117fa
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-05 02:02:01 +0800

    test: Add test for isNotANumber function

---
+++ test/isNotANumber.test.ts
@@ -0,0 +1,18 @@
+import { isNotANumber } from "../src/validation-fucntions";
+
+describe("isNotANumber", () => {
+  test("Should return true if the data passed is NOT a number.", () => {
+    const data = "Foo" as unknown;
+    expect(isNotANumber(data)).toBe(true);
+  });
+
+  test("Should return true if the data passed is a string of a number.", () => {
+    const data = "69" as unknown;
+    expect(isNotANumber(data)).toBe(true);
+  });
+
+  test("Should return false if the data passed IS a number.", () => {
+    const data = 69 as unknown;
+    expect(isNotANumber(data)).toBe(false);
+  });
+});
