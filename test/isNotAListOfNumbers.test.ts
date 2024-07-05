4544e5088d6dd9f1aa6f7c1983be3a8695e597c8
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-05 17:07:18 +0800

    test: Add test for helper function isNotAListOfNumbers

---
+++ test/isNotAListOfNumbers.test.ts
@@ -0,0 +1,23 @@
+import { isNotAListOfNumbers } from "../src/validation-fucntions";
+
+describe("isNotAListOfNumbers", () => {
+  test("Should return false if given an array of numbers", () => {
+    const list = [1, 2, 3, 4, 5] as unknown;
+    expect(isNotAListOfNumbers(list)).toBe(false);
+  });
+
+  test("Should return true if given an array of strings", () => {
+    const list = ["foo", "bar"] as unknown;
+    expect(isNotAListOfNumbers(list)).toBe(true);
+  });
+
+  test("Should return true if given an array of booleans", () => {
+    const list = [true, false, false] as unknown;
+    expect(isNotAListOfNumbers(list)).toBe(true);
+  });
+
+  test("Should return true given an array of objects", () => {
+    const list = [{ foo: "Bar" }];
+    expect(isNotAListOfNumbers(list)).toBe(true);
+  });
+});
