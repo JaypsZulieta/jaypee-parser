15b8f2c5f6cc49a759cef144cad1f4436fc1bac0
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-05 17:14:12 +0800

    test: Add test for helper function isNotAListOfBooleans

---
+++ test/isNotAListOfBooleans.test.ts
@@ -0,0 +1,23 @@
+import { isNotAListOfBooleans } from "../src/validation-fucntions";
+
+describe("isNotAListOfBooleans", () => {
+  test("Should return false given a list of booleans", () => {
+    const list = [true, false, true, false] as unknown;
+    expect(isNotAListOfBooleans(list)).toBe(false);
+  });
+
+  test("Should return true given a list of numbers", () => {
+    const list = [1, 2, 3, 4, , 5, 6, 7] as unknown;
+    expect(isNotAListOfBooleans(list)).toBe(true);
+  });
+
+  test("Should return true given a list of strings", () => {
+    const list = ["Foo", "Bar", "Baz"] as unknown;
+    expect(isNotAListOfBooleans(list)).toBe(true);
+  });
+
+  test("Should return true given a list of objects", () => {
+    const list = [{ foo: "Bar" }];
+    expect(isNotAListOfBooleans(list)).toBe(true);
+  });
+});
