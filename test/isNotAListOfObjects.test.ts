bd71738682dd15f98ff92596d88e1fb2e07f7e19
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-05 17:21:20 +0800

    test: Add test for helper function isNotAListOfObjects

---
+++ test/isNotAListOfObjects.test.ts
@@ -0,0 +1,23 @@
+import { isNotAListOfObjects } from "../src/validation-fucntions";
+
+describe("isNotAListOfObjects", () => {
+  test("Should return false if given a list of objects", () => {
+    const list = { foo: "Bar" } as unknown;
+    expect(isNotAListOfObjects(list)).toBe(false);
+  });
+
+  test("Should return true if given a list of numbers", () => {
+    const list = [1, 2, 3, 4, 5] as unknown;
+    expect(isNotAListOfObjects(list)).toBe(true);
+  });
+
+  test("Should return true if given a list of strings", () => {
+    const list = ["Foo", "Bar"] as unknown;
+    expect(isNotAListOfObjects(list)).toBe(true);
+  });
+
+  test("Should return true if given a list of booleans", () => {
+    const list = [true, false, true] as unknown;
+    expect(isNotAListOfObjects(list)).toBe(true);
+  });
+});
