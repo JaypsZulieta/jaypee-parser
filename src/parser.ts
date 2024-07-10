53b8ff775be815bdadee6fc357a1a0f9e0956197
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-10 18:54:47 +0800

    doc: Add jsdoc comments to the string methods in the Parser class
    Add jsdoc comments to give the user the recommended use-cases for the
    method and to inform them of the exceptions that can be thrown by it,
    in a descriptive way.

--- src/parser.ts
+++ src/parser.ts
@@ -16,6 +16,15 @@ export class Parser {
     return new Parser(data);
   }
 
+  /**
+   * Use this to extract the value of a specified key and parse it into a
+   * string so that it can be used with the assuarance of type-safety.
+   * @throws {NullFieldValidationException} if the the value of the field is null
+   * @throws {UndefinedFieldValidationException} if the field is undefined
+   * @throws {StringValidationException} if the value of the field is not a string
+   * @param keyPath the full path of the key to be extracted
+   * @returns the extracted value as a string
+   */
   getString(keyPath: string): string {
     const fieldValue = getValueByPath(this.data, keyPath);
     if (fieldValue === null) throw new NullFieldValidationException(keyPath);
@@ -24,11 +33,26 @@ export class Parser {
     return fieldValue as string;
   }
 
+  /**Use this to extract the value of a specified key and parse it into a string with the
+   * posibility of it also being undefined so that it can be used with the assurance of
+   * type-safety
+   * @throws {NullFieldValidationException} if the value of the field is null
+   * @throws {StringValidationException} if the field exists but it is not a string
+   * @param keyPath the full path of the key to be extracted
+   * @returns the extracted value as a string or as undefined if the field does not exist
+   */
   getStringOrUndefined(keyPath: string): string | undefined {
     const fieldValue = getValueByPath(this.data, keyPath);
     return fieldValue === undefined ? undefined : this.getString(keyPath);
   }
 
+  /**Use this to extract the value of a specified key and parse it into a string with the
+   * posibility of it also being null so that it can be used with the assurance of type-safety.
+   * @throws {UndefinedFieldValidationException} if the field is undefined
+   * @throws {StringValidationException} if the field is not null or is not a string
+   * @param keyPath the full path of the key to be extracted
+   * @returns the extracted value as a string or null
+   */
   getStringOrNull(keyPath: string): string | null {
     const fieldValue = getValueByPath(this.data, keyPath);
     return fieldValue === null ? null : this.getString(keyPath);
