ad7f7427aaedfbe69c57e7d60250ebbf30d45767
Jaypee Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-07-15 16:19:11 +0800

    test: Add tests for Parser.getBoolean

---
+++ test/Parser/Parser.getBoolean.test.ts
@@ -0,0 +1,47 @@
+import { Parser } from "../../src/parser";
+import {
+  BooleanValidationException,
+  NullFieldValidationException,
+  UndefinedFieldValidationException,
+} from "../../src/validation-exception";
+
+describe("Parser.getBoolean", () => {
+  test("Should throw a NullFieldValidationException if the value is null", () => {
+    const parser = Parser.parseFrom({ isHandsome: null });
+    const action = () => {
+      parser.getBoolean("isHandsome");
+    };
+    expect(action).toThrow(NullFieldValidationException);
+  });
+
+  test("Should throw a UndefinedFieldValidationException if the value is unedined", () => {
+    const parser = Parser.parseFrom({});
+    const action = () => {
+      parser.getBoolean("isHandsome");
+    };
+    expect(action).toThrow(UndefinedFieldValidationException);
+  });
+
+  test("Should throw a BooleanValidationException if the value is not a boolean", () => {
+    const parser = Parser.parseFrom({ isHandsome: 69 });
+    const action = () => {
+      parser.getBoolean("isHandsome");
+    };
+    expect(action).toThrow(BooleanValidationException);
+  });
+
+  test("Should return a boolean", () => {
+    const parser = Parser.parseFrom({ isHandsome: true });
+    expect(parser.getBoolean("isHandsome")).toBe(true);
+  });
+
+  test("Should return true if the value is 1", () => {
+    const parser = Parser.parseFrom({ isHandsome: 1 });
+    expect(parser.getBoolean("isHandsome")).toBe(true);
+  });
+
+  test("Should return false if the value is 0", () => {
+    const parser = Parser.parseFrom({ isHandsome: 0 });
+    expect(parser.getBoolean("isHandsome")).toBe(false);
+  });
+});
